21887553f87700ec76a10401621d5835c3d5b140
manoncarbonnel <manon.carbonnel@gmail.com>
2014-02-05 16:22:09 +0100

    regroupement v1

---
+++ sources/project/php/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+	
+		<meta charset="utf-8" />
+		
+		<title>Contact</title>
+		
+		<!--IMPORTANT : ces link sont temporaire pour la mise en forme de la page contact et du footer, il sont à supprimer une fois que Valérian les aura fusionné dans ces fichiers-->
+		<link rel="stylesheet" type="text/css" href="miseenforme.css" />
+		
+		<link rel="stylesheet" type="text/css" href="footer.css" />
+		
+	</head>
+	
+	<body>
+		
+		<!-- Intégration du bandeau des réseaux sociaux -->
+		<?php
+			include ("reseauxSociaux.html")
+		?>
+
+		<!-- Intégration du bandeau -->
+		<?php
+			include ("bandeau.html");
+		?>
+		
+		<!-- Informations de contact -->
+		<div class="alignement" id="">
+		
+		Leonnie<br />
+		<br />
+		Céline Ronsin<br />
+		06 75 35 07 53<br />
+		</div>
+		
+		<!-- Formulaire de contact -->
+		<div class="" id="">
+		
+		<!-- Ici sera inclu le formulaire de contact de Lionel -->
+		
+		</div>
+		
+		<!-- Intégration du footer le footer n'intègre pas les fermetures body et html-->
+		<?php
+			include ("footer.html");
+		?>
+	</body>
+
+</html>
