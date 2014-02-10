3aa31d34b0194545d70a423a1b5beed00907d5f1
LucienBrahmard <maxime.itstart@imie-rennes.fr>
2014-02-10 16:58:25 +0100

    La galerie normalement bien ficelée

---
+++ sources/pages/Maxime/galerie/galerie.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<?php include ("bandeau.html"); ?>
+<link rel="stylesheet" type="text/css" href="galerie.css" />
+
+<script type="text/javascript" src="galerie.js"></script>
+
+<script src="lightbox/js/modernizr.custom.js"></script>
+<script src="lightbox/js/jquery-1.10.2.min.js"></script>
+<script src="lightbox/js/lightbox-2.6.min.js"></script>
+<link href="lightbox/css/lightbox.css" rel="stylesheet" />
+</head>
+
+<body>
+<div id="galerie">
+  <ul id="galerie_mini">
+    
+    <a href="images/01.jpg"  data-lightbox="roadtrip"><img src="images/01.jpg" /></a>    
+    
+    <a href="images/02.jpg"  data-lightbox="roadtrip"><img src="images/02.jpg" /></a>    
+    
+    <a href="images/03.jpg"  data-lightbox="roadtrip"><img src="images/03.jpg" /></a>    
+	  
+    <a href="images/04.jpg"  data-lightbox="roadtrip"><img src="images/04.jpg" /></a>    
+    
+    <a href="images/05.jpg"  data-lightbox="roadtrip"><img src="images/05.jpg" /></a>
+
+	<a href="images/06.jpg"  data-lightbox="roadtrip"><img src="images/06.jpg" /></a>	  </ul>
+
+  
+</div>
+</body>
+</html>
