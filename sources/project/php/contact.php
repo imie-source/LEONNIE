f328df266ea1bdc630b66ed5d6e5ffbaaebddba6
JeremyGicquel <sombracier56@hotmail.fr>
2014-02-14 13:57:56 +0100

    Modification des pages PHP, création de pages html.

--- sources/project/php/contact.php
+++ sources/project/php/contact.php
@@ -1,51 +1,22 @@
-<!DOCTYPE html>
+<?php
 
-<html>
-
-	<head>
+	/* Intégration du bandeau */
 	
-		<meta charset="utf-8" />
-		<title>Contact</title>
-			<!--IMPORTANT : ces link sont temporaire pour la mise en forme de la page contact et du footer, il sont à supprimer une fois que Valérian les aura fusionné dans ces fichiers-->
-		<link rel="stylesheet" type="text/css" href="../css/miseenforme.css" />
-		<link rel="stylesheet" type="text/css" href="../css/footer.css" />
-		
-	</head>
+	include ("../html/bandeau.html");
+
 	
-	<body>
-		
-		<!-- Intégration du bandeau des réseaux sociaux -->
-		<?php
-		//	include ("../html/reseauxSociaux.html")
-		?>
+	/* Intégration du contenu de la page */
+	
+	include ("../html/contact.html");
+	
+	
+	/* Formulaire de contact de Lionel */
 
-		<!-- Intégration du bandeau -->
-		<?php
-			include ("../html/bandeau.html");
-		?>
-		
-		<!-- Informations de contact -->
-		<div class="alignement" id="">
-		
-		Leonnie<br />
-		<br />
-		Céline Ronsin<br />
-		06 75 35 07 53<br />
-		</div>
-		
-		<!-- Formulaire de contact -->
-		<div class="" id="">
-		
-		<!-- Ici sera inclu le formulaire de contact de Lionel -->
-		<?php
-			include ("../html/form.html");
-		?>
-		</div>
-		
-		<!-- Intégration du footer le footer n'intègre pas les fermetures body et html-->
-		<?php
-			include ("../html/footer.html");
-		?>
-	</body>
-
-</html>
+	include ("../html/form.html");
+	
+	
+	/* Intégration du footer */
+	
+	include ("../html/footer.html");
+	
+?>
---
+++ sources/project/php/decoration.php
@@ -0,0 +1,18 @@
+<?php
+	
+	/* Intégration du bandeau */
+	
+	include ("../html/bandeau.html");
+	
+	
+	/* Intégration du contenu de la page */
+	
+	include ("../html/decoration.html");
+	
+	
+	/* Intégration du footer */
+	
+	include ("../html/footer.html");
+
+
+?>
