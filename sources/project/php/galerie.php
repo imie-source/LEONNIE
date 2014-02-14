b08ce554ab89c54de3e57a2de4158a63e7382ada
manoncarbonnel <manon.carbonnel@gmail.com>
2014-02-14 11:22:49 +0100

    Zblaaa

---
+++ sources/project/php/galerie.php
@@ -0,0 +1,33 @@
+
+'<html>
+<head>
+<?php include ("../html/bandeau.html"); ?>
+<link rel="stylesheet" type="text/css" href="../css/galerie.css" />
+
+<script src="../js/lightbox/js/modernizr.custom.js"></script>
+<script src="../js/lightbox/js/jquery-1.10.2.min.js"></script>
+<script src="../js/lightbox/js/lightbox-2.6.min.js"></script>
+<link href="../js/lightbox/css/lightbox.css" rel="stylesheet" />
+</head>
+
+<body>
+<form method="POST" action="test.php" enctype="multipart/form-data">
+<div id="galerie">
+  <ul id="galerie_mini">
+    
+    <a href="../medias/images_galerie/01.jpg"  name="image1" data-lightbox="roadtrip"><img src="../medias/images_galerie/01.jpg" /></a>    
+    
+    <a href="../medias/images_galerie/02.jpg"  name="image2" data-lightbox="roadtrip"><img src="../medias/images_galerie/02.jpg" /></a>    
+    
+    <a href="../medias/images_galerie/03.jpg"  name="image2" data-lightbox="roadtrip"><img src="../medias/images_galerie/03.jpg" /></a>    
+	  
+    <a href="../medias/images_galerie/04.jpg"  name="image2" data-lightbox="roadtrip"><img src="../medias/images_galerie/04.jpg" /></a>    
+    
+    <a href="../medias/images_galerie/05.jpg"  name="image2" data-lightbox="roadtrip"><img src="../medias/images_galerie/05.jpg" /></a>
+
+	<a href="../medias/images_galerie/06.jpg"  name="image2" data-lightbox="roadtrip"><img src="../medias/images_galerie/06.jpg" /></a>	  </ul>
+  
+</div>
+</form>
+</body>
+</html>'
