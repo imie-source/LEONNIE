474f8b26baf839a4319d3307ae2e364793493ccb
LucienBrahmard <maxime.itstart@imie-rennes.fr>
2014-02-14 10:24:10 +0100

    Voici la nouvelle galerie qui marche bien!!

--- sources/pages/Maxime/galerie/galerie.php
+++ sources/pages/Maxime/galerie/galerie.php
@@ -1,4 +1,5 @@
-<html>
+<?php
+'<html>
 <head>
 <?php include ("bandeau.html"); ?>
 <link rel="stylesheet" type="text/css" href="galerie.css" />
@@ -12,22 +13,24 @@
 </head>
 
 <body>
+<form method="POST" action="test.php" enctype="multipart/form-data">
 <div id="galerie">
   <ul id="galerie_mini">
     
-    <a href="images/01.jpg"  data-lightbox="roadtrip"><img src="images/01.jpg" /></a>    
+    <a href="images/01.jpg"  name="image1" data-lightbox="roadtrip"><img src="images/01.jpg" /></a>    
     
-    <a href="images/02.jpg"  data-lightbox="roadtrip"><img src="images/02.jpg" /></a>    
+    <a href="images/02.jpg"  name="image2" data-lightbox="roadtrip"><img src="images/02.jpg" /></a>    
     
-    <a href="images/03.jpg"  data-lightbox="roadtrip"><img src="images/03.jpg" /></a>    
+    <a href="images/03.jpg"  name="image2" data-lightbox="roadtrip"><img src="images/03.jpg" /></a>    
 	  
-    <a href="images/04.jpg"  data-lightbox="roadtrip"><img src="images/04.jpg" /></a>    
+    <a href="images/04.jpg"  name="image2" data-lightbox="roadtrip"><img src="images/04.jpg" /></a>    
     
-    <a href="images/05.jpg"  data-lightbox="roadtrip"><img src="images/05.jpg" /></a>
+    <a href="images/05.jpg"  name="image2" data-lightbox="roadtrip"><img src="images/05.jpg" /></a>
 
-	<a href="images/06.jpg"  data-lightbox="roadtrip"><img src="images/06.jpg" /></a>	  </ul>
-
+	<a href="images/06.jpg"  name="image2" data-lightbox="roadtrip"><img src="images/06.jpg" /></a>	  </ul>
   
 </div>
+</form>
 </body>
-</html>
+</html>'
+?>
