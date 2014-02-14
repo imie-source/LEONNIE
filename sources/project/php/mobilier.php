e26cf1396a36b5c3eede717391b8fc37214d7587
JeremyGicquel <sombracier56@hotmail.fr>
2014-02-14 15:23:48 +0100

    Intégration des pages boutiques

---
+++ sources/project/php/mobilier.php
@@ -0,0 +1,17 @@
+<?php
+
+	/* Intégration du bandeau */
+	
+	include ("../html/bandeau.html");
+
+	
+	/* Intégration du contenu de la page */
+	
+	include ("../html/mobilier.html");
+	
+	
+	/* Intégration du footer */
+	
+	include ("../html/footer.html");
+	
+?>
